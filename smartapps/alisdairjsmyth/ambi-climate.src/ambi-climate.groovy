a18e9cedd3643ee35316785cc7a32af500db4335
Alisdair Smyth <alisdair.smyth@gmail.com>
2017-01-11 22:30:14 +1100

    Initial commit

--- smartapps/alisdairjsmyth/ambi-climate.src/ambi-climate.groovy
+++ smartapps/alisdairjsmyth/ambi-climate.src/ambi-climate.groovy
@@ -12,30 +12,83 @@ definition(
     category: "SmartThings Labs",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    singleInstance: false)
+{
+	appSetting "bearerToken"
+}
 
 
 preferences {
-	section("Title") {
-		// TODO: put inputs here
+	section("Parameters") {
+    	input (name: "mac", type: "text", title: "Device MAC")
+        input (name: "roomName", type: "text", title: "Room Name")
+        input (name: "locationName", type: "text", title: "Location Name")
 	}
 }
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
-
 	initialize()
 }
 
 def updated() {
 	log.debug "Updated with settings: ${settings}"
-
-	unsubscribe()
 	initialize()
 }
 
 def initialize() {
 	// TODO: subscribe to attributes, devices, locations, etc.
+
+	// Delete Existing Child Device
+    def devices = getChildDevices()
+    devices.each {
+    	deleteChildDevice(it.deviceNetworkId)
+    }
+    
+    // Retrieve Current Values
+    def resp = getDeviceState()
+    if (resp.status == 200) {
+    	def data = [
+        	name: settings.roomName,
+            temperature: resp.data.value
+        ]
+        log.debug "Device data: ${data}"
+
+        device = addChildDevice("alisdairjsmyth", "Ambi Climate", settings.mac, null, data)
+    }
+    
+    // Schedule Every 10 Minutes
+	runEvery10Minutes(poll)
+}
+
+def uninstalled() {
+	// Delete Existing Child Device
+    def devices = getChildDevices()
+    devices.each {
+    	deleteChildDevice(it.deviceNetworkId)
+    }
+}
+
+def getDeviceState() {
+	def params = [
+    	uri: "https://api.ambiclimate.com/api/v1",
+        path: "/device/sensor/temperature",
+        query: [
+        	access_token: appSettings.bearerToken,
+            room_name: settings.roomName,
+            location_name: settings.locationName
+        ]
+    ]
+    
+    try {
+	    httpGet(params) {resp ->
+        	return resp;
+        }
+   	} catch (groovyx.net.http.HttpResponseException e) {
+		return e.response
+    }
+    
 }
 
 // TODO: implement event handlers
