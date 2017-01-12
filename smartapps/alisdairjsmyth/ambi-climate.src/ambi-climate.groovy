5dccea9b13d0356ffcb814cd82d5b078a5a9afaa
Alisdair Smyth <alisdair.smyth@gmail.com>
2017-01-13 08:51:23 +1100

    Added dynamic page which lists available Ambi Climate devices

--- smartapps/alisdairjsmyth/ambi-climate.src/ambi-climate.groovy
+++ smartapps/alisdairjsmyth/ambi-climate.src/ambi-climate.groovy
@@ -15,16 +15,83 @@ definition(
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     singleInstance: false)
 {
+	appSetting "email"
+    appSetting "password"
 	appSetting "bearerToken"
 }
 
-
 preferences {
-	section("Parameters") {
-    	input (name: "mac", type: "text", title: "Device MAC")
-        input (name: "roomName", type: "text", title: "Room Name")
-        input (name: "locationName", type: "text", title: "Location Name")
-	}
+    page(name:"deviceDiscovery", title:"Device Selection", content:"deviceDiscovery")
+}
+
+private deviceDiscovery() {
+	def options = deviceOptions() ?: []
+    def count   = options.size()
+
+    return dynamicPage(name:"deviceDiscovery", title:"Device Selection", nextPage:"", refreshInterval: refreshInterval, install:true, uninstall: true) {
+        section("Select Ambi Climate devices to add...") {
+            input "selectedDevices", "enum", required:true, title:"Blinds (${count} found)", multiple:true, options:options
+        }
+    }
+}
+
+Map deviceOptions() {
+	def options = [:]
+    def devices = getDevices()
+    devices.data.devices.each { device ->
+    	options[device.mac] = device.room_name
+    }
+    log.debug("Options: ${options}")
+    return options
+}
+
+def getDevices() {
+    def response
+    Map params
+
+	// Authenticate
+    params = [
+   		uri: "https://rest.ambiclimate.com",
+       	path: "/UserCredential",
+        query: [
+   	    	email: appSettings.email,
+       	    pwd: appSettings.password
+        ]
+   	]
+    
+   	try {
+   		httpGet(params) { resp ->
+       		log.debug("Authentication Response: ${resp.data}")
+
+            response = resp
+   	    }
+    } catch (groovyx.net.http.HttpResponseException e) {
+		throw e
+    }
+    
+    // Retrieve List of Ambi Climate Devices
+    params = [
+    	uri: "https://rest.ambiclimate.com",
+        path: "/User",
+        query: [
+        	expand: "appliance%2Cdevice",
+            user_id: resp.data.user_id
+        ],
+        headers: [
+        	Authorization: "Bearer ${resp.data.token_id}"
+        ]
+    ]
+    
+    try {
+    	httpGet(params) { resp ->
+        	log.debug("Retrieve List Response: ${resp.data}")
+            return resp
+        }
+    } catch (groovyx.net.http.HttpResponseException e) {
+		throw e
+    }
+    
+
 }
 
 def installed() {
@@ -39,56 +106,19 @@ def updated() {
 
 def initialize() {
 	// TODO: subscribe to attributes, devices, locations, etc.
-
-	// Delete Existing Child Device
-    def devices = getChildDevices()
-    devices.each {
-    	deleteChildDevice(it.deviceNetworkId)
-    }
-    
-    // Retrieve Current Values
-    def resp = getDeviceState()
-    if (resp.status == 200) {
-    	def data = [
-        	name: settings.roomName,
-            temperature: resp.data.value
-        ]
-        log.debug "Device data: ${data}"
-
-        device = addChildDevice("alisdairjsmyth", "Ambi Climate", settings.mac, null, data)
-    }
-    
-    // Schedule Every 10 Minutes
-	runEvery10Minutes(poll)
+    updateDevices()
 }
 
 def uninstalled() {
 	// Delete Existing Child Device
-    def devices = getChildDevices()
-    devices.each {
-    	deleteChildDevice(it.deviceNetworkId)
-    }
+
 }
 
-def getDeviceState() {
-	def params = [
-    	uri: "https://api.ambiclimate.com/api/v1",
-        path: "/device/sensor/temperature",
-        query: [
-        	access_token: appSettings.bearerToken,
-            room_name: settings.roomName,
-            location_name: settings.locationName
-        ]
-    ]
-    
-    try {
-	    httpGet(params) {resp ->
-        	return resp;
-        }
-   	} catch (groovyx.net.http.HttpResponseException e) {
-		return e.response
+def updateDevices() {
+	if (!state.devices) {
+    	state.devices = [:]
     }
+    def selectors = []
     
-}
 
-// TODO: implement event handlers
+}
